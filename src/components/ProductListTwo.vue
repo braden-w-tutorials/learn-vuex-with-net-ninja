637bae598dab2af4c41fc4e7710a6a352d604a39
Braden <bmw02002turbo@gmail.com>
2021-01-20 16:20:44 -0800

    added productlisttwo.vue

---
+++ src/components/ProductListTwo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="product-list-two">
+    <h2>Product List Two</h2>
+    <ul>
+      <li v-for=""></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        { name: "Banana Skin", price: 20 },
+        { name: "Shiny Star", price: 20 },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
