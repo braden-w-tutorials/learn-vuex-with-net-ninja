dfefacf4fb9602f632cd4799570eee87b5a66f28
Braden <bmw02002turbo@gmail.com>
2021-01-20 16:20:29 -0800

    Added import

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
 </template>
 
 <script>
+import ProductListOne from "./components/ProductListOne.vue"
+import ProductListTwo from "./components/ProductListTwo.vue"
 export default {
   name: "App",
   data(){
