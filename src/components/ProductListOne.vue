c0db44ea68dca827f653fe904661c8b3f513690f
Braden <bmw02002turbo@gmail.com>
2021-01-20 16:28:50 -0800

    Enabled props, deleted data

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -3,18 +3,18 @@
     <h2>Product List One</h2>
     <ul>
       <li v-for="product in products" :key="product">
+        <span class="name">{{product.name}}</span>
+        <span class="price">{{product.price}}</span>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
+  props: ["products"],
   data() {
     return {
-      products: [
-        { name: "Banana Skin", price: 20 },
-        { name: "Shiny Star", price: 20 },
-      ],
     }
   },
 }
