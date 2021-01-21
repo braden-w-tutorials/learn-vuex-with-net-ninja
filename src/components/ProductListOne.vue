a75fed90fc4cff28e3580b5ff77a1b5a0a1b58fb
Braden <bmw02002turbo@gmail.com>
2021-01-20 16:20:38 -0800

    Added productlistone.vue

---
+++ src/components/ProductListOne.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="product-list-one">
+    <h2>Product List One</h2>
+    <ul>
+      <li v-for=""></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        { name: "Banana Skin", price: 20 },
+        { name: "Shiny Star", price: 20 },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
